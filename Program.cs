cd8dd690181b0f32878720e3bf720fa304f37201
JoaoPedroCordoba <jp.8pedro86@gmail.com.br>
2022-03-21 02:16:54 -0300

    finalizado exercicio

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿double C, F;
+
+Console.WriteLine("Digite a temperatura em Graus °C, por favor:");
+C = Convert.ToDouble(Console.ReadLine());
+
+F = C * 1.8 + 32;
+
+Console.WriteLine($"A temperatura em °F é {F}°F");
